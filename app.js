1aef125009ec0a6fb302d755ba00c6a3a0cf9145
Juan Sierra <jasierra.garcia@gmail.com>
2018-01-09 22:55:07 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.get('/', (req, res) => res.sendFile('/mnt/c/Users/juan/Documents/Game/index.html'));
+
+app.listen(3000);
